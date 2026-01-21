a4b866293a7126fd97303b76ae31594ec9ba9435
rezafauzan593@gmail.com <rezafauzan593@gmail.com>
2026-01-21 16:57:48 +0700

    Init: Initialize project

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import { createRoot } from "react-dom/client";
+import './assets/css/style.css'
+
+const root = createRoot(document.getElementById('root'))
+
+
+
+root.render()
---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import react from "@vitejs/plugin-react"
+import tailwind from "@tailwindcss/vite"
+import { defineConfig } from "vite"
+
+export default defineConfig({
+    plugins: [
+        react(),
+        tailwind()
+    ]
+})
