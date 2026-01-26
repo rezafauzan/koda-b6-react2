e300e4d6935dc2364b985ebd94577c116f7a10d7
rezafauzan593@gmail.com <rezafauzan593@gmail.com>
2026-01-26 15:09:33 +0700

    Feat: add JSDoc on button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,21 @@
+/**
+ * @typedef {object} ButtonProps
+ * @property {"blue"?} theme
+ */
+
+/**
+ * 
+ * @typedef {React.ButtonHTMLAttributes<HTMLButtonElement> & ButtonProps} HTMLButtonProps 
+ */
+
+/**
+ * @param {HTMLButtonProps} props 
+ * @returns {import("react").JSX.Element}
+ */
 const Button = (props) => {
+    const { theme, ...buttonDefault } = props
     return (
-        <button className="bg-blue-500 p-4 text-white rounded shadow-lg cursor-pointer" onClick={props.callback}>{props.children}</button>
+        <button className={(theme === "blue" ? "bg-blue-400 text-white " : "bg-white ") + "border border-black/40 p-4  rounded shadow-xl cursor-pointer"} {...buttonDefault}>{props.children}</button>
     )
 }
 
